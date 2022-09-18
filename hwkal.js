7ac77d7b27073f94b21ac4837dc751a8ae59e82d
VilewSja <111483311+VilewSja@users.noreply.github.com>
2022-09-18 21:31:10 +0800

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,13 +13,13 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/HwModsWa857'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6285714170944'
+global.youtube = 'https://youtube.com/channel/UCJ2UvqI3wU0tdANH_hxvNnQ'
+global.ig = 'https://instagram.com/levirillnofek?igshid=YmMyMTA2M2Y='
+global.mygc = 'wa.me/6281255090574'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
+global.email = 'gaada dek'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Levi' //ubah jadi nama mu, note tanda ' gausah di hapus!
